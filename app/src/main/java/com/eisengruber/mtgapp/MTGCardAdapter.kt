ae06aa5177038666a3f172b44db01b849ef652a2
Andrew <maskedvigilandy@gmail.com>
2022-08-09 15:20:48 -0400

    We made an adapter

---
+++ app/src/main/java/com/eisengruber/mtgapp/MTGCardAdapter.kt
@@ -0,0 +1,36 @@
+package com.eisengruber.mtgapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+
+class MTGCardAdapter(
+    var cardList: List<Card>,
+    var context: Context) : Adapter<MTGCardAdapter.CardViewHolder>() {
+
+    class CardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        var textViewCardName : TextView = itemView.findViewById(R.id.textViewCardName)
+        var textViewCMCList : TextView = itemView.findViewById(R.id.textViewCMC)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
+        return CardViewHolder(LayoutInflater.from(context).inflate(R.layout.card_design,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
+        var card = cardList[position]
+        holder.textViewCardName.text = card.cardName
+        holder.textViewCMCList.text = card.convertedManaCost.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return cardList.size
+    }
+
+}
--- app/src/main/java/com/eisengruber/mtgapp/MainActivity.kt
+++ app/src/main/java/com/eisengruber/mtgapp/MainActivity.kt
@@ -3,15 +3,25 @@ package com.eisengruber.mtgapp
 import RetrofitClientInstance.retrofitInstance
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+
+lateinit var recyclerView: RecyclerView
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //This is the new stuff added 8/1/2022
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
 
         // Get the cards from internet
         callGetCards()
@@ -40,8 +50,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun doSomethingWithCardData(card: List<Card>) {
-        val testName = card.first().cardName
-        testName
+    private fun doSomethingWithCardData(cards: List<Card>) {
+        val adapter = MTGCardAdapter(cards, applicationContext)
+        recyclerView.adapter = adapter
     }
 }
